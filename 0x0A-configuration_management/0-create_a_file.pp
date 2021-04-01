fd489720f67e03ec53b5552dda49eb4795b7681c
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-01 18:25:29 +0000

    First commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file
+file { '/tmp/holberton':
+  ensure  =>  file,
+  owner   =>  www-data,
+  group   =>  www-data,
+  mode    =>  '0744',
+  content =>  'I love Puppet',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,3 @@
+# Kill a process
+exec { '/usr/bin/env pkill -9 killmenow':
+}
