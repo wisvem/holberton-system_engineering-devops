0ce1eaced251ea70fff15d38ef658cfdce114264
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-06 18:15:01 +0000

    Added puppet conf file

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,30 @@
+# Config file
+exec { 'update':
+    command => '/usr/bin/apt-get update -y'
+}
+exec { 'install-nginx':
+    command => '/usr/bin/apt-get install -y'
+}
+exec { 'echo-holb':
+    command => '/bin/echo Holberton School > /var/www/html/index.nginx-debian.html'
+}
+exec { 'echo-404':
+    command => '/bin/echo Ceci n\'est pas une page > /var/www/html/404.html'
+}
+$my_string = "\\\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n"
+$myfile = '/etc/nginx/sites-available/default'
+$error404="\\\n\terror_page 404 /404.html;\n\tlocation /404.html {\n\t\tinternal;\n\t}\n";
+
+file_line {'redirect':
+    match   => $my_string,
+    path    => $myfile,
+    line    => $my_string,
+    replace => false
+}
+
+file_line {'redirect':
+    match   => $error404,
+    path    => $myfile,
+    line    => $error404,
+    replace => false
+}
