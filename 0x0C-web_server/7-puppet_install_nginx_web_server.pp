0bc3370b198f6e3230f72356a3014854dcceda84
Wiston Venera Macias <wisvem@hotmail.com>
2021-04-06 23:18:40 +0000

    ANother puppet conf

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,18 @@
 # Manifest to configure an Ubuntu server with nginx
-
 package { 'nginx':
   ensure   => present,
   provider => 'apt'
 }
-
 file { '/var/www/html/index.html':
   ensure  => present,
   content => 'Holberton School'
 }
-
 file_line { 'Redirection':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
 }
-
 service { 'nginx':
   ensure    => running,
   enable    => true,
