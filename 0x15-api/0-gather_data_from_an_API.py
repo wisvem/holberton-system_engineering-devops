1ffc470e44fcb5e5b8eb61cd0f49a5b438a3dcbb
Wiston Venera Macías <wisvem@hotmail.com>
2021-05-02 23:39:26 +0000

    Added if __name__(__main__) to prvent execution when imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 """ 0. Gather data from an API """
-from sys import argv
 import requests
+from sys import argv
 
+if __name__ == "__main__":
+    url_todo = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        argv[1])
+    url_user = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    todo_response = requests.get(url_todo)
+    user_response = requests.get(url_user)
+    todo_json = todo_response.json()
+    user_name = user_response.json().get('name')
 
-url_todo = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-    argv[1])
-url_user = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
-todo_response = requests.get(url_todo)
-user_response = requests.get(url_user)
-todo_json = todo_response.json()
-user_name = user_response.json().get('name')
+    done_list = []
+    done_count = 0
+    task_count = 0
+    for i in todo_json:
+        task_count += 1
+        if i.get('completed'):
+            done_list.append(i.get('title'))
+            done_count += 1
 
-done_list = []
-done_count = 0
-task_count = 0
-for i in todo_json:
-    task_count += 1
-    if i.get('completed'):
-        done_list.append(i.get('title'))
-        done_count += 1
+    td = "{}/{}".format(done_count, task_count)
 
-td = "{}/{}".format(done_count, task_count)
-
-print("Employee {} is donde with tasks ({}):".format(user_name, td))
-for task in done_list:
-    print('\t {}'.format(task))
+    print("Employee {} is donde with tasks ({}):".format(user_name, td))
+    for task in done_list:
+        print('\t {}'.format(task))
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,24 +3,23 @@
 import requests
 from sys import argv
 
+if __name__ == "__main__":
+    url_todo = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+        argv[1])
+    url_user = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    todo_response = requests.get(url_todo)
+    user_response = requests.get(url_user)
+    todo_json = todo_response.json()
+    username = user_response.json().get('username')
 
-url_todo = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-    argv[1])
-url_user = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
-todo_response = requests.get(url_todo)
-user_response = requests.get(url_user)
-todo_json = todo_response.json()
-username = user_response.json().get('username')
+    task_list = []
 
-task_list = []
+    for i in todo_json:
+        tmp = "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(
+            i.get('userId'), username, i.get('completed'), i.get('title'))
+        print(tmp)
+        task_list.append(tmp)
 
-for i in todo_json:
-    tmp = "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(
-        i.get('userId'), username, i.get('completed'), i.get('title'))
-    print(tmp)
-    task_list.append(tmp)
-
-
-with open('USER_ID.csv', 'w') as f:
-    for i in task_list:
-        f.writelines(i)
+    with open('USER_ID.csv', 'w') as f:
+        for i in task_list:
+            f.writelines(i)
