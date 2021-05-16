172a085cf9e00c113f3a5b2725436e47dbb23323
Wiston Venera Macías <wisvem@hotmail.com>
2021-05-16 21:35:57 +0000

    Task 3

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" 0. How many subs? """
+
+from requests import get
+
+headers = {'User-Agent': 'My User Agent'}
+
+
+def validate_word_dict(word_dict, word_list):
+    """Validate word dict"""
+    for word in word_list:
+        if word.lower() not in word_dict:
+            word_dict[word.lower()] = 0
+    return word_dict
+
+
+def count_words(subreddit, word_list):
+    result = recursive_count(subreddit, word_list, '', {})
+    for k, v in sorted(result.items(), key=lambda x: (-x[1], x[0])):
+        if v is not 0:
+            print(k, v)
+    return None
+
+
+def recursive_count(subreddit, word_list, after='', word_dict={}):
+    """Number of subs function"""
+    word_dict = validate_word_dict(word_dict, word_list)
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+        subreddit, after)
+    response = get(url, headers=headers, allow_redirects=False)
+    if response.status_code is not 200:
+        return None
+    r = response.json()
+    data = r.get('data')
+    children = data['children']
+    title_list = []
+    # get every title
+    for i in range(len(children)):
+        title_list.append(children[i].get('data')['title'].lower())
+
+    for title in title_list:
+        for key in word_dict.keys():
+            for x in title.split(' '):
+                if key == x:
+                    word_dict[key] += 1
+
+    after = data['after']
+    if after is not None:
+        recursive_count(subreddit, word_list, after, word_dict)
+    return word_dict
