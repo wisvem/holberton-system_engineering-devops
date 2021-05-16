a3015b75b9fc00aa25f1016fa3be750274e45f5f
Wiston Venera Macías <wisvem@hotmail.com>
2021-05-16 16:46:24 +0000

    First commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 0. How many subs? """
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """Number of subs function"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    r = get(url).json()
+    if r.get('kind') == 'Listing':
+        return 0
+    data = r.get('data')
+    return data['subscribers']
