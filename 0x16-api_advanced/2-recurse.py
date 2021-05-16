66e22aefe59b62c442272c3d57d56ac21677b04d
Wiston Venera Macías <wisvem@hotmail.com>
2021-05-16 19:08:31 +0000

    Task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,15 +6,20 @@ from requests import get
 headers = {'User-Agent': 'My User Agent'}
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=""):
     """Number of subs function"""
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+        subreddit, after)
     response = get(url, headers=headers, allow_redirects=False)
     if response.status_code is not 200:
-        return print('None')
-
+        return hot_list
     r = response.json()
     data = r.get('data')
     children = data['children']
-    for i in range(10):
-        print(children[i].get('data')['title'])
+    for i in range(len(children)):
+        hot_list.append(children[i].get('data')['title'])
+    after = data['after']
+    if after is not None:
+        recurse(subreddit, hot_list, after)
+    return hot_list
