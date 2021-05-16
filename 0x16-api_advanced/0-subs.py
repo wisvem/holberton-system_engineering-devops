9b2cfb903c75f82ca6cab56103bfd31a6a8e56ad
Wiston Venera Macías <wisvem@hotmail.com>
2021-05-16 16:50:14 +0000

    Added condition when invalid subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     """Number of subs function"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     r = get(url).json()
-    if r.get('kind') == 'Listing':
+    if r.get('kind') != 't5':
         return 0
     data = r.get('data')
     return data['subscribers']
