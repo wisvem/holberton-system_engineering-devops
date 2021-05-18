b826967e8a693224fabbf1d2bd61b63dee9f95a8
Wiston Venera Macías <wisvem@hotmail.com>
2021-05-18 00:42:29 +0000

    Task 0 - Removed ensure

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,4 @@
 # Manifest to configure an Ubuntu server with nginx
-file_line { 'Check if file exist':
-  ensure => present,
-  path   => '/var/www/html/wp-settings.php',
-}
-->
 exec { 'fix type error':
   command  => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
   user     => 'root',
